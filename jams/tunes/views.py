ba4ea29ce69d8b065f32c692a4f4783d1a8df9e2
mkrs90 <katy.simpson@student.awesomeinc.org>
2023-04-03 20:21:06 +0000

    set up and tested genres

---
+++ jams/tunes/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Genre
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from rest_framework import permissions
+from .serializers import UserSerializer, GroupSerializer, GenreSerializer
+
+# Create your views here.
+
+def view_genre(request, genre_id):
+    print(genre_id)
+    selected_genre = Genre.objects.get(pk = genre_id)
+    print(selected_genre)
+    return HttpResponse('<h1>Name: %s </h1>' % selected_genre.name)
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
+class GenreViewSet(viewsets.ModelViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
